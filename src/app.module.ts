0d12914154b9d5cd0ed550158474cea83a998c68
jhonntantb <jhonntantb@gmail.com>
2022-06-04 11:16:38 -0500

    database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com',
+      port: 3306,
+      username: 'bsale_test',
+      password: 'bsale_test',
+      database: 'bsale_test',
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
